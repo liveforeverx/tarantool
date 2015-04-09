9000b5846a587a0a76d88fef233e6d5f1c8bbf2f
Sulverus <sulverus@gmail.com>
2015-04-09 08:47:22 +0000

    Crash test with replication in a loop

--- test/recovery/all.test.py
+++ test/recovery/all.test.py
@@ -1,23 +1,50 @@
 import os
+import time
+from lib.tarantool_server import TarantoolServer
 
 engines = ['memtx', 'sophia']
+master = server
 
 # for each engine insert test data
 command = 'insert_tests(box.space.%s_space)'
-results = dict((e, server.admin(command % e)) for e in engines)
+results = dict((e, master.admin(command % e)) for e in engines)
  
 # make snapshot
-server.admin('box.snapshot()')
+master.admin('box.snapshot()')
 
-# stop and start server
-print 'Crashing tarantool'
-server.stop()
+print 'Join replica'
+master_id = master.get_param('server')['id']
+lsn = master.get_lsn(master_id)
+replica = TarantoolServer(master.ini)
+replica.script = 'recovery/replica.lua'
+replica.vardir = os.path.join(master.vardir, 'replica')
+replica.rpl_master = master
+replica.deploy()
+replica.wait_lsn(master_id, lsn)
 
-print 'Start tarantool again'
-server.start()
+replica.admin('box.snapshot()')
+nodes = [master, replica]
 
-# check data
-for engine in engines:
-    check = server.admin('check(box.space.%s_space)' % engine)
-    print check == results[engine]
+print '---------------[Starting crash test loop]----------------'
+for i in xrange(0, 10):
+    # stop and start nodes
+    print 'Crashing tarantool'
+    for node in nodes:
+        node.stop()
 
+    print 'Start tarantool again'
+    for node in nodes:
+        node.start()
+    master_id = master.get_param('server')['id']
+    lsn = master.get_lsn(master_id)
+    #replica.admin('require("fiber").info()')
+    replica.wait_lsn(master_id, lsn)
+    
+    # check data
+    for node in nodes:
+        node.admin('box.info().server.id')
+        for engine in engines:
+            check = node.admin('check(box.space.%s_space)' % engine)
+            print check == results[engine]
+    results = dict((e, master.admin(command % e)) for e in engines)
+    print '---------------[End of crash iteration %d]----------------' % (i + 1)
--- test/recovery/recovery.lua
+++ test/recovery/recovery.lua
@@ -5,9 +5,15 @@ box.cfg{
     listen              = os.getenv("LISTEN"),
     slab_alloc_arena    = 0.1,
     pid_file            = "tarantool.pid",
-    rows_per_wal        = 50
+    logger              = "| cat - >> tarantool.log",
+    rows_per_wal        = 50,
+    custom_proc_title   = "master",
 }
 require('console').listen(os.getenv('ADMIN'))
+
+pcall(function()
+    box.schema.user.grant('guest', 'replication')
+end)
 
 test_config = {
     memtx='memtx_space',
@@ -26,10 +32,25 @@ function check(space)
     return space:select{}
 end
 
+if box.space.counter == nil then
+    local cnt_space = box.schema.create_space('counter')
+    cnt_space:create_index('primary', {unique = true, parts = {1, 'STR'}})
+end
+
+counter = box.space.counter:select{'cnt'}[1]
+
+if not counter then
+    box.space.counter:insert{'cnt', 0}
+end
+
+
 function insert_tests(space)
-    for i = 1, 10 do
+    local right = box.space.counter:select{'cnt'}[1][2] + 1
+    local left = right + 1
+    for i = right, left do
         space:insert({i, 'test_data ' .. tostring(i), i * 2})
     end
+    box.space.counter:replace{'cnt', left}
     return check(space)
 end
 
